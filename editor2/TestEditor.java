5c56b6e9f26b755c0f322e1a1f57c5bb6f394f1a
knppkp <140078295+knppkp@users.noreply.github.com>
2025-07-26 20:52:35 +0700

    Add files via upload

--- editor2/TestEditor.java
+++ editor2/TestEditor.java
@@ -3,27 +3,20 @@ package editor2;
 public class TestEditor {
 
     public static void main(String[] args) {
-        DocumentInvoker myDocument1 = new DocumentInvoker("callmemaybe");
-        myDocument1.Write("Hey I just met you");
-        myDocument1.Write("And this is crazy");
-        myDocument1.Bold(1);
-        myDocument1.Write("But here's my number");
-        myDocument1.Redo();
-        myDocument1.Undo(0);
-        myDocument1.Write("So call me maybe");
-        myDocument1.Italic(3);
-        myDocument1.Insert(2, "test inert");
-        myDocument1.Redo();
-        myDocument1.Undo();
-        System.out.println(myDocument1.Read());
-        myDocument1.printCommandBuffer();
-
-        DocumentInvoker myDocument2 = new DocumentInvoker("callmemaybe");
-        myDocument2.Write("Hey I just met you");
-        myDocument2.Write("And this is crazy");
-        myDocument2.Write("But here's my number");
-        myDocument2.Undo(2);
-        System.out.println(myDocument2.Read());
-        myDocument2.printCommandBuffer();
+        DocumentInvoker myDocument = new DocumentInvoker("callmemaybe");
+        myDocument.Write("Hey I just met you");
+        myDocument.Write("And this is crazy");
+        myDocument.Bold(1);
+        myDocument.Write("But here's my number");
+        myDocument.Redo();
+        myDocument.Undo(0);
+        myDocument.Write("So call me maybe");
+        myDocument.Italic(3);
+        myDocument.Redo(4);
+        myDocument.Insert(2, "test inert");
+        myDocument.Redo();
+        myDocument.Undo();
+        System.out.println(myDocument.Read());
+        myDocument.printCommandBuffer();
     }
 }
